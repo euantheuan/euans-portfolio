8c8480e0b27a8a715a5cbdcd24fc589461a7b156
유안 <euantheuan@gmail.com>
2024-11-19 02:36:56 +0900

    skills added

--- js/index.js
+++ js/index.js
@@ -1,31 +1,58 @@
-// Register GSAP ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
 // Horizontal scrolling sections
 const horizontalScroll = () => {
-    let horizontalSections = gsap.utils.toArray(".horizontal section");
+    const horizontalSections = gsap.utils.toArray(".horizontal section");
+    const horizontalWrapper = document.querySelector(".horizontal");
 
-    gsap.to(horizontalSections, {
+    const horizontalTween = gsap.to(horizontalSections, {
         xPercent: -100 * (horizontalSections.length - 1),
         ease: "none",
         scrollTrigger: {
             trigger: ".horizontal",
             pin: true,
+            pinSpacing: true,
             scrub: 1,
             snap: {
                 snapTo: 1 / (horizontalSections.length - 1),
                 duration: 0.5,
                 ease: "power1.inOut"
             },
-            end: () => `+=${document.querySelector(".horizontal").offsetWidth}`,
-            markers: true
+            end: () => `+=${horizontalWrapper.offsetWidth}`,
+            markers: true,
+            onLeaveBack: () => {
+                gsap.to(horizontalWrapper, {
+                    visibility: "visible",
+                    duration: 0
+                });
+            }
         }
+    });
+
+    // Create a trigger for hiding horizontal section
+    ScrollTrigger.create({
+        trigger: ".vertical",
+        start: "top bottom",
+        end: "top top",
+        onEnter: () => {
+            gsap.to(horizontalWrapper, {
+                visibility: "hidden",
+                duration: 0
+            });
+        },
+        onLeaveBack: () => {
+            gsap.to(horizontalWrapper, {
+                visibility: "visible",
+                duration: 0
+            });
+        },
+        markers: true
     });
 };
 
 // Vertical scrolling sections
 const verticalScroll = () => {
-    let verticalSections = gsap.utils.toArray(".vertical section");
+    const verticalSections = gsap.utils.toArray(".vertical section");
 
     verticalSections.forEach((section, i) => {
         ScrollTrigger.create({
@@ -33,7 +60,7 @@ const verticalScroll = () => {
             start: "top top",
             pin: true,
             pinSpacing: false,
-            snap: 1, // Snap to sections
+            snap: 1,
             markers: true
         });
     });
